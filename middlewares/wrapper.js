c6a4f7417644d1c719ada049e63cf4a05d779e7e
Kevin Wang <rocwang0316@gmail.com>
2018-12-26 11:38:45 -0800

    Added the cors and functionShield middlewares from middy library.

--- middlewares/wrapper.js
+++ middlewares/wrapper.js
@@ -4,10 +4,23 @@
  * A middleware to wrap some comman middlwares. So all function will have these middlewares automatically.
  */
 const middy = require('middy');
+const { cors, functionShield, ssm } = require('middy/middlewares');
 
 const sampleLogging = require('./sample-logging');
-const functionShield = require('./function-shield');
+// const functionShield = require('./function-shield');
 
 module.exports = func => middy(func)
+  .use(cors({
+    origin: 'https://kairoscope.resonancepath.com',
+    credentials: true,
+  }))
+  .use(ssm({ FUNCTION_SHIELD_TOKEN: '/kairoscope/dev/function_shield_token' }))
   .use(sampleLogging())
-  .use(functionShield);
+  .use(functionShield({
+    policy: {
+      outbound_connectivity: 'alert',
+      read_write_tmp: 'block',
+      create_child_process: 'block',
+      read_handler: 'block',
+    },
+  }));
