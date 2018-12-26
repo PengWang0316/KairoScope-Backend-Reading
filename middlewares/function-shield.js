4dc366785bf59b920620a99fcd1686535922114a
Kevin Wang <rocwang0316@gmail.com>
2018-12-25 16:25:06 -0800

    Setup a variable to just call FunctionShield.config once.

--- middlewares/function-shield.js
+++ middlewares/function-shield.js
@@ -2,20 +2,25 @@
 
 const FuncShield = require('@puresec/function-shield');
 
+var isFuncShieldSetup = false;
+
 module.exports = {
   before: (handler, next) => {
-    FuncShield.configure({ // Use the Function Shield to block outbound, read, write, and create child process activities
-      policy: {
-        // 'block' mode => active blocking
-        // 'alert' mode => log only
-        // 'allow' mode => allowed, implicitly occurs if key does not exist
-        outbound_connectivity: 'alert',
-        read_write_tmp: 'block',
-        create_child_process: 'block',
-        read_handler: 'block',
-      },
-      token: process.env.FUNCTION_SHIELD_TOKEN,
-    });
+    if (!isFuncShieldSetup) {
+      FuncShield.configure({ // Use the Function Shield to block outbound, read, write, and create child process activities
+        policy: {
+          // 'block' mode => active blocking
+          // 'alert' mode => log only
+          // 'allow' mode => allowed, implicitly occurs if key does not exist
+          outbound_connectivity: 'alert',
+          read_write_tmp: 'block',
+          create_child_process: 'block',
+          read_handler: 'block',
+        },
+        token: process.env.FUNCTION_SHIELD_TOKEN,
+      });
+      isFuncShieldSetup = true;
+    }
     next();
   },
 };
