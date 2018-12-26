725933f0fcba45ab2747d1ca3ff08421cf5acd99
Kevin Wang <rocwang0316@gmail.com>
2018-12-25 23:28:55 -0800

    Added the integration test and acceptance test.

---
+++ __tests__/test_cases/fetch-readings-amount.js
@@ -0,0 +1,24 @@
+import { invokeFetchReadingsAmount } from '../helpers/InvokeHelper';
+import initEvns from '../helpers/InitialEnvs';
+
+let context;
+
+describe('get-index: invoke the Get / endpoint', () => {
+  beforeAll(async () => {
+    jest.setTimeout(10000); // Setup a longer timeout to allow CodeBuild fetch the credantial keys from ECS.
+    await initEvns();
+    context = {
+      dbUrl: process.env['db-host'],
+      dbName: process.env['db-name'],
+      jwtSecret: process.env['jwt-secret'],
+    };
+  });
+
+  test('invoke fetch-readings-amount function', async () => {
+    const event = { queryStringParameters: { jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0F1dGgiOnRydWUsInJvbGUiOjIsIl9pZCI6IjU5ZGU5ZTUwMjM1NDNmOGEyOGNmYzA3MSIsImlhdCI6MTU0NTc2NjQ0MH0.ZJ9nXFbfuYo73SQAGal_NYi9aeAwNfR_X45527VAopc' } };
+    const res = await invokeFetchReadingsAmount(event, context);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBe(21);
+  });
+});
