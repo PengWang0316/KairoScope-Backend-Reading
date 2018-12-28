6f7040ea48d116f774af6a663cb88bff6e4636d0
Kevin Wang <rocwang0316@gmail.com>
2018-12-28 11:08:37 -0800

    Added the integration and acceptance test for the fetch-all-reading-list function.

---
+++ __tests__/test_cases/fetch-all-reading-list.js
@@ -0,0 +1,44 @@
+import { invokeFetchAllReadingList } from '../helpers/InvokeHelper';
+import initEvns from '../helpers/InitialEnvs';
+
+let context;
+
+describe('fetch-all-reading-list: invoke the Get / endpoint', () => {
+  beforeAll(async () => {
+    jest.setTimeout(10000); // Setup a longer timeout to allow CodeBuild fetch the credantial keys from ECS.
+    await initEvns();
+    context = {
+      dbUrl: process.env['db-host'],
+      dbName: process.env['db-name'],
+      jwtSecret: process.env['jwt-secret'],
+    };
+  });
+
+  test('invoke fetch-all-reading-list function', async () => {
+    const event = {
+      queryStringParameters: {
+        jwtMessage: process.env.jwt,
+        pageNumber: '1',
+        numberPerpage: '10',
+      },
+    };
+    const res = await invokeFetchAllReadingList(event, context);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBe(10);
+    res.body.forEach(reading => {
+      expect(Object.prototype.hasOwnProperty.call(reading, '_id')).toBe(true);
+      expect(Object.prototype.hasOwnProperty.call(reading, 'reading_name')).toBe(true);
+      expect(Object.prototype.hasOwnProperty.call(reading, 'hexagram_arr_1')).toBe(true);
+      expect(Object.prototype.hasOwnProperty.call(reading, 'hexagram_arr_2')).toBe(true);
+      expect(Object.prototype.hasOwnProperty.call(reading, 'img1')).toBe(true);
+      expect(Object.prototype.hasOwnProperty.call(reading, 'img2')).toBe(true);
+      expect(Object.prototype.hasOwnProperty.call(reading, 'date')).toBe(true);
+      expect(Object.prototype.hasOwnProperty.call(reading, 'change_lines')).toBe(true);
+      expect(Object.prototype.hasOwnProperty.call(reading, 'change_lines_text')).toBe(true);
+      expect(Object.prototype.hasOwnProperty.call(reading, 'people')).toBe(true);
+      expect(Object.prototype.hasOwnProperty.call(reading, 'userName')).toBe(true);
+      expect(Object.prototype.hasOwnProperty.call(reading, 'user_id')).toBe(true);
+    });
+  });
+});
