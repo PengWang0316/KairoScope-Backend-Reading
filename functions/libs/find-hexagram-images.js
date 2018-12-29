3ebebe2dd80c432e0230fbc0e221f873a7692080
Kevin Wang <rocwang0316@gmail.com>
2018-12-28 16:12:40 -0800

    Separated some common code to libraries.

---
+++ functions/libs/find-hexagram-images.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { getDB } = require('../../libs/MongoDBHelper');
+
+const { hexagramCollectionName } = process.env;
+// TODO should be refactored to use Redux match the result from the front-end
+/* Working with method below to execute the callback function when all hexagram are fetched. */
+const checkHexagramImageReadAndCallback = (checkNumber, targetNumber, callback, result) => {
+  if (checkNumber === targetNumber) callback(result);
+};
+
+/** This method is using to find hexagram information for readings
+  * @param {array} readings is an array that has reading objects.
+  * @param {function} callback is a function will be transfered to anther function.
+  * @return {null} No return.
+ */
+const findHexagramImages = (readings, callback) => {
+  let checkNumber = 0;
+  const targetNumber = readings.length * 2;
+  // Making a copy for the readings. So, the code below is safe when change reading in the forEach function.
+  const copyReadings = [...readings];
+  copyReadings.forEach(reading => {
+    getDB().collection(hexagramCollectionName)
+      .find({ img_arr: reading.hexagram_arr_1 }).next((err, imgInfo) => {
+        reading.img1Info = imgInfo;
+        checkNumber += 1;
+        checkHexagramImageReadAndCallback(checkNumber, targetNumber, callback, copyReadings);
+      });
+    getDB().collection(hexagramCollectionName)
+      .find({ img_arr: reading.hexagram_arr_2 }).next((err, imgInfo) => {
+        reading.img2Info = imgInfo;
+        checkNumber += 1;
+        checkHexagramImageReadAndCallback(checkNumber, targetNumber, callback, copyReadings);
+      });
+  });
+};
+
+module.exports = findHexagramImages;
---
+++ functions/libs/search-for-readings.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const log = require('../../libs/log');
+const { getDB } = require('../../libs/MongoDBHelper');
+const findHexagramImages = require('./find-hexagram-images');
+
+const { readingCollectionName } = process.env;
+/** Working with method below to search readings based on the hexagram.
+  * @param {object} query is an object that has reading's information that a user wants to search.
+  * @param {function} callback is the function that will be executed after this function's call.
+  * @param {object} results is the object that comes from hexagram search.
+  * @return {null} No return.
+*/
+function searchForReadings(query, callback, results) {
+  // assemble query object for MongoDB
+  const queryArray = [];
+  if (query.people) queryArray.push({ people: new RegExp(`.*${query.people}.*`) });
+  if (query.userId) queryArray.push({ user_id: query.userId });
+  if (results) {
+    // if no img_arr was found, it means not such combination exsite. Give a empty array and quit.
+    if (results.length === 0) {
+      callback([]);
+      return;
+    }
+    // if users used hexagrams' criterias, add img_arr for the searching criteria
+    const hexagramQuery = [];
+    results.forEach(element => {
+      hexagramQuery.push({ hexagram_arr_1: element.img_arr });
+      hexagramQuery.push({ hexagram_arr_2: element.img_arr });
+    });
+    queryArray.push({ $or: hexagramQuery });
+  }
+  // Start to deal with start date and end date
+  if (query.endDate) {
+    const endDate = new Date(query.endDate);
+    endDate.setDate(endDate.getDate() + 1);
+    queryArray.push({
+      $and: [{ date: { $gte: new Date(query.startDate) } }, { date: { $lt: new Date(endDate) } }],
+    });
+  } else if (query.startDate) {
+    /* If just one date is given, set the search criteria between that day's 00:00 to next day's 00:00 */
+    const endDate = new Date(query.startDate);
+    endDate.setDate(endDate.getDate() + 1);
+    queryArray.push({
+      $and: [{ date: { $gte: new Date(query.startDate) } }, { date: { $lt: endDate } }],
+    });
+  }
+  if (queryArray.length === 0) queryArray.push({}); // if no one searching criteria was given, give a empty array to query, which will pull out all readings.
+
+  getDB().collection(readingCollectionName)
+    .find({ $and: queryArray }).sort({ date: -1 })
+    .toArray((err, result) => {
+      if (err) log.error('searchForReadings: ', err);
+      if (result.length !== 0) findHexagramImages(result, callback);
+      else callback(result);
+    });
+}
+
+module.exports = searchForReadings;
