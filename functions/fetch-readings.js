77c9addb965c017bb6b3aae36be52195a018bc2e
Kevin Wang <rocwang0316@gmail.com>
2018-12-29 14:47:12 -0800

    Updated the code to handle the error condition.

--- functions/fetch-readings.js
+++ functions/fetch-readings.js
@@ -9,17 +9,18 @@ const findHexagramImages = require('./libs/find-hexagram-images');
 const { readingCollectionName } = process.env;
 
 const handler = async (event, context, callback) => {
-  const userRole = context.user.role || 3; // Give a default role
   const { pageNumber, numberPerpage } = event.queryStringParameters;
   const result = await cloudwatch.trackExecTime('MongoDBFindLatency', () => new Promise((resolve, reject) => {
     getDB().collection(readingCollectionName)
-      .find(userRole * 1 === process.env.ADMINISTRATOR_ROLE * 1 ? {} : { user_id: context.user._id })
+      .find(context.user.role * 1 === process.env.ADMINISTRATOR_ROLE * 1 ? {} : { user_id: context.user._id })
       .sort({ date: -1 })
       .limit(numberPerpage * 1)
       .skip(pageNumber * numberPerpage)
       .toArray((err, readingResult) => {
-        if (err) log.error('Reading getRecentReadings something goes worry: ', err);
-        if (readingResult.length !== 0) findHexagramImages(readingResult, backResult => resolve(backResult));
+        if (err) {
+          log.error(`Reading getRecentReadings something goes worry: ${err}`);
+          resolve({});
+        } else if (readingResult.length !== 0) findHexagramImages(readingResult, backResult => resolve(backResult));
         else resolve(readingResult);
       });
   }));
