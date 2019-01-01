8a403e90115f8525e4a0f4f553261cbc85bef31d
Kevin Wang <rocwang0316@gmail.com>
2018-12-31 17:14:03 -0800

    Added the fetch-readings-by-name function.

---
+++ functions/fetch-readings-by-name.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const wrapper = require('../middlewares/wrapper');
+const { promiseFindResult } = require('../libs/MongoDBHelper');
+const cloudwatch = require('../libs/cloudwatch');
+
+const { readingCollectionName } = process.env;
+
+const handler = async (event, context, callback) => {
+  const { keyWord } = event.queryStringParameters;
+  const result = await cloudwatch.trackExecTime('MongoDBFindLatency', () => promiseFindResult(db => db
+    .collection(readingCollectionName)
+    .find({ user_id: context.user._id, reading_name: new RegExp(`.*${keyWord}.*`, 'i') }, { projection: { reading_name: 1, date: 1 } })
+    .limit(10)
+    .sort({ date: -1 })));
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(result),
+  });
+};
+
+module.exports.handler = wrapper(handler);
