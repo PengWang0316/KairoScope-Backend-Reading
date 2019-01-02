158e7bd34b58d5400b2d668b356d1989b12ae5dc
Kevin Wang <rocwang0316@gmail.com>
2019-01-02 11:29:01 -0800

    Added the delete-journal function.

---
+++ functions/delete-journal.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { ObjectId } = require('mongodb');
+
+const wrapper = require('../middlewares/wrapper');
+const { promiseInsertResult } = require('../libs/MongoDBHelper');
+const cloudwatch = require('../libs/cloudwatch');
+
+const { readingCollectionName } = process.env;
+
+const handler = async (event, context, callback) => {
+  const { journalId, readingIds } = JSON.parse(event.body);
+  await cloudwatch.trackExecTime('MongoDbUpdateLatancy', () => promiseInsertResult(db => db
+    .collection(readingCollectionName)
+    .update(
+      { _id: { $in: readingIds.map(id => new ObjectId(id)) }, user_id: context.user._id },
+      { $pull: { journal_entries: { _id: new ObjectId(journalId) } } },
+      { multi: true },
+    )));
+
+  callback(null, {
+    statusCode: 200,
+    body: 'ok',
+  });
+};
+module.exports.handler = wrapper(handler);
