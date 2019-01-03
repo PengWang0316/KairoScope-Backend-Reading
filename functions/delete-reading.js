751c61163a91c81efedf38dd55cf5d09018bb5da
Kevin Wang <rocwang0316@gmail.com>
2019-01-03 14:02:03 -0800

    Added the delete-reading function.

---
+++ functions/delete-reading.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { ObjectId } = require('mongodb');
+
+const wrapper = require('../middlewares/wrapper');
+const { promiseInsertResult } = require('../libs/MongoDBHelper');
+const cloudwatch = require('../libs/cloudwatch');
+
+const { readingCollectionName } = process.env;
+
+const handler = async (event, context, callback) => {
+  const { readingId } = event.queryStringParameters;
+  await cloudwatch.trackExecTime('MongoDbDeleteOneLatancy', () => promiseInsertResult(db => db
+    .collection(readingCollectionName)
+    .deleteOne({
+      _id: new ObjectId(readingId),
+      user_id: context.user._id,
+    })));
+
+  callback(null, {
+    statusCode: 200,
+    // body: {},
+  });
+};
+module.exports.handler = wrapper(handler);
