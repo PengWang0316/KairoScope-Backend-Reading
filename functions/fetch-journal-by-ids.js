1e623ebba357277c02cd969e8f87267393662998
Kevin Wang <rocwang0316@gmail.com>
2019-01-03 17:09:44 -0800

    Updated the code to handle the error

--- functions/fetch-journal-by-ids.js
+++ functions/fetch-journal-by-ids.js
@@ -5,25 +5,33 @@ const { ObjectId } = require('mongodb');
 const wrapper = require('../middlewares/wrapper');
 const { getDB } = require('../libs/MongoDBHelper');
 const cloudwatch = require('../libs/cloudwatch');
+const log = require('../libs/log');
 
 const { readingCollectionName } = process.env;
 
 const handler = async (event, context, callback) => {
   const { journalId, readingId } = event.queryStringParameters;
-  const result = await cloudwatch.trackExecTime('MongoDBFindLatency', () => new Promise((resolve, reject) => getDB()
-    .collection(readingCollectionName)
-    .findOne({ _id: new ObjectId(readingId), user_id: context.user._id }, { journal_entries: 1 })
-    .then((readings, err) => {
-      if (err) reject(err);
-      else readings.journal_entries.forEach(journal => {
-        if (journal._id.toString() === journalId) resolve(journal);
-      });
-    })));
+  try {
+    const result = await cloudwatch.trackExecTime('MongoDBFindOneLatency', () => new Promise((resolve, reject) => getDB()
+      .collection(readingCollectionName)
+      .findOne({ _id: new ObjectId(readingId), user_id: context.user._id }, { journal_entries: 1 })
+      .then((reading, err) => {
+        if (err) reject(err);
+        else reading.journal_entries.forEach(journal => {
+          if (journal._id.toString() === journalId) resolve(journal);
+        });
+      })));
 
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(result),
-  });
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    });
+  } catch (error) {
+    log.error(`Error message from fetch-journal-by-ids: ${error}`);
+    callback(null, {
+      statusCode: 500,
+    });
+  }
 };
 
 module.exports.handler = wrapper(handler);
