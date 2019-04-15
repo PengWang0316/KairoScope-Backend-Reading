9ce65afb75a9ea780f9dd72ee9e8623456cdcbe2
Kevin Wang <rocwang0316@gmail.com>
2019-04-14 17:15:07 -0700

    Added capability to read from Redis

--- functions/fetch-readings.js
+++ functions/fetch-readings.js
@@ -1,18 +1,21 @@
 'use strict';
 
-const wrapper = require('../middlewares/wrapper');
 const { getDB } = require('@kevinwang0316/mongodb-helper');
 const log = require('@kevinwang0316/log');
 const cloudwatch = require('@kevinwang0316/cloudwatch');
+const wrapper = require('../middlewares/wrapper');
 const findHexagramImages = require('./libs/find-hexagram-images');
 
 const { readingCollectionName } = process.env;
 
 const handler = async (event, context, callback) => {
   const { pageNumber, numberPerpage } = event.queryStringParameters;
+  const {
+    user, redisHost, redisPort, redisPassword,
+  } = context;
   const result = await cloudwatch.trackExecTime('MongoDBFindLatency', () => new Promise((resolve, reject) => {
     getDB().collection(readingCollectionName)
-      .find(context.user.role * 1 === process.env.ADMINISTRATOR_ROLE * 1 ? {} : { user_id: context.user._id })
+      .find(user.role * 1 === process.env.ADMINISTRATOR_ROLE * 1 ? {} : { user_id: user._id })
       .sort({ date: -1 })
       .limit(numberPerpage * 1)
       .skip(pageNumber * numberPerpage)
@@ -20,8 +23,9 @@ const handler = async (event, context, callback) => {
         if (err) {
           log.error(`Reading getRecentReadings something goes worry: ${err}`);
           resolve({});
-        } else if (readingResult.length !== 0) findHexagramImages(readingResult, backResult => resolve(backResult));
-        else resolve(readingResult);
+        } else if (readingResult.length !== 0) findHexagramImages(
+          redisHost, redisPort, redisPassword, readingResult, backResult => resolve(backResult),
+        ); else resolve(readingResult);
       });
   }));
   callback(null, {
--- functions/libs/find-hexagram-images.js
+++ functions/libs/find-hexagram-images.js
@@ -1,8 +1,14 @@
 'use strict';
 
 const { getDB } = require('@kevinwang0316/mongodb-helper');
+const {
+  createClient, getAsync, quit,
+} = require('@kevinwang0316/redis-helper');
+const log = require('@kevinwang0316/log');
+const cloudwatch = require('@kevinwang0316/cloudwatch');
 
-const { hexagramCollectionName } = process.env;
+
+const { hexagramCollectionName, redisKeyHexagrams } = process.env;
 // TODO should be refactored to use Redux match the result from the front-end
 /* Working with method below to execute the callback function when all hexagram are fetched. */
 const checkHexagramImageReadAndCallback = (checkNumber, targetNumber, callback, result) => {
@@ -14,7 +20,8 @@ const checkHexagramImageReadAndCallback = (checkNumber, targetNumber, callback, 
   * @param {function} callback is a function will be transfered to anther function.
   * @return {null} No return.
  */
-const findHexagramImages = async (readings, callback) => {
+const findHexagramImagesFromDB = async (readings, callback) => {
+  log.debug('Fall back to db read');
   let checkNumber = 0;
   const targetNumber = readings.length * 2;
   // Making a copy for the readings. So, the code below is safe when change reading in the forEach function.
@@ -33,6 +40,29 @@ const findHexagramImages = async (readings, callback) => {
         checkHexagramImageReadAndCallback(checkNumber, targetNumber, callback, copyReadings);
       });
   });
+};
+
+const findHexagramImages = async (redisHost, redisPort, redisPassword, readings, callback) => {
+  try {
+    log.debug('Initializing Redis client');
+    createClient(redisHost, redisPort, redisPassword);
+    const hexagrams = await cloudwatch.trackExecTime('RedisGetLatency', () => getAsync(redisKeyHexagrams));
+    // Fall back to read from the db if the Redis does not have the info
+    if (hexagrams === null) await findHexagramImagesFromDB(readings, callback);
+    else {
+      log.debug('Cache hit');
+      readings.forEach(reading => {
+        reading.img1Info = hexagrams[reading.hexagram_arr_1];
+        reading.img2Info = hexagrams[reading.hexagram_arr_2];
+      });
+      callback(readings);
+    }
+  } catch (err) { // Fall back to read from the db if the Redis throws error
+    log.error(err);
+    await findHexagramImagesFromDB(readings, callback);
+  } finally {
+    quit();
+  }
 };
 
 module.exports = findHexagramImages;
